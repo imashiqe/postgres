9d023b5d40eebe43475960e9673cec976159b511
Ashiqur Rahman <imashiqe@gmail.com>
2025-02-18 11:13:42 +0600

    Exploring Stored Procedure in PostgreSQL

--- functionProcedures.sql
+++ functionProcedures.sql
@@ -1,3 +1,4 @@
+
 SELECT * from employees
 
 SELECT count(*) from employees;
@@ -9,5 +10,22 @@ RETURNS INT
 LANGUAGE SQL
 AS
 $$
-   SELECT COUNT(*) FROM employees;
+   DELETE FROM employees  WHERE  employee_id = p_emp_id;
 $$;
+
+
+CREATE Procedure remove_emp()
+LANGUAGE plpgsql
+
+AS
+$$
+DECLARE
+test_var int;
+BEGIN 
+     SELECT employee_id INTO test__var from  employees WHERE employee_id = p_emp_id;
+     DELETE FROM employees  WHERE  employee_id = test_var;
+     RAISE NOTICE 'Employee with id % has been removed' ;
+END
+ $$;
+
+ call remove_emp();
